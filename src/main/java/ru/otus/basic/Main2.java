a6f431611b8dfc55bdf71383670013b762e72cfd
alekskiseleva <sasha4800309@hotmail.com>
2023-07-12 18:55:53 +0300

    init commit2

---
+++ src/main/java/ru/otus/basic/Main2.java
@@ -0,0 +1,13 @@
+package ru.otus.basic;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое число");
+        int result = scanner.nextInt();
+        System.out.println(result);
+    }
+}
+
