2286097f2b1d4cd1f450fa51868e3f2f6298e008
alekskiseleva <sasha4800309@hotmail.com>
2023-08-14 09:23:03 +0300

    init commit7

--- src/main/java/homework5/Animal.java
+++ src/main/java/homework5/Animal.java
@@ -5,45 +5,50 @@ public class Animal {
     protected double runningRate;
     protected double swimmingRate;
     protected int endurance;
+    protected double runEnduranceCoefficient;
+    protected double swimEnduranceCoefficient;
 
-    public Animal(String name, double runningRate, double swimmingRate, int endurance) {
+    public Animal(String name, double runningRate, double swimmingRate, int endurance, double runEnduranceCoefficient, double swimEnduranceCoefficient) {
         this.name = name;
         this.runningRate = runningRate;
         this.swimmingRate = swimmingRate;
         this.endurance = endurance;
+        this.runEnduranceCoefficient = runEnduranceCoefficient;
+        this.swimEnduranceCoefficient = swimEnduranceCoefficient;
     }
 
     public double run(int distance) {
-
-        if (endurance < distance) {
-            System.out.println(name + " не хватает выносливости для пробежки.");
-            return -1;
-        }
 
         if (runningRate == 0) {
             System.out.println(name + " не умеет бегать.");
             return -1;
         }
 
+        if (endurance < distance * runEnduranceCoefficient) {
+            System.out.println(name + " не хватает выносливости для пробежки.");
+            return -1;
+        }
+
         double time = distance / runningRate;
-        endurance -= distance;
+        endurance -= distance * runEnduranceCoefficient;
         System.out.println(name + " пробежал " + distance + " метров за " + time + " секунд");
         return time;
     }
 
     public double swim(int distance) {
 
-        if (endurance < distance) {
-            System.out.println(name + " не хватает выносливости для плаванья.");
-            return -1;
-        }
-
         if (swimmingRate == 0) {
             System.out.println(name + " не умеет плавать.");
             return -1;
         }
 
+        if (endurance < distance * swimEnduranceCoefficient) {
+            System.out.println(name + " не хватает выносливости для плаванья.");
+            return -1;
+        }
+
         double time = distance / swimmingRate;
+        endurance -= distance * swimEnduranceCoefficient;
         System.out.println(name + " проплыл " + distance + " метров за " + time + " секунд");
         return time;
     }
